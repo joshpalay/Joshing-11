a80d0d4ca76335d502e461277c4af8d31f045d55
joshpalay <joshuapalay@gmail.com>
2026-06-05 10:06:12 -0400

    Compact authored question rows

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -435,7 +435,7 @@ function QuestionsPageContent() {
               </button>
             </section>
           ) : (
-            <section className="space-y-3">
+            <section className="space-y-2">
               {filteredQuestions.map((question) => (
                 <MyQuestionCard
                   key={question.id}
